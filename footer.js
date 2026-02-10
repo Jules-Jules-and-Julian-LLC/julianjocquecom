05c21befcca9a14d1a6851fdc50eeefb3dfd9fb2
Julian Jocque <lamabuny@gmail.com>
2026-02-09 23:10:17 -0600

    implement footers, add SHREK!

---
+++ footer.js
@@ -0,0 +1,10 @@
+document.currentScript.insertAdjacentHTML('beforebegin',
+  '<div class="other-creations">' +
+    '<a href="https://writeshite.com/" target="_blank" rel="noopener noreferrer" class="writeshite-logo-link" title="WriteShite - Collaborative Writing Game">' +
+      '<span class="writeshite-text">Write</span><span class="writeshite-poop" role="img" aria-label="smiling poop">\uD83D\uDCA9</span>' +
+    '</a>' +
+    '<a href="https://ptatotp.clickclickclickclickclickclickclickclickclickclickclick.click/" target="_blank" rel="noopener noreferrer" class="ptatotp-link" title="PTATOTP">' +
+      '<span class="ptatotp-mini"><span>P</span><span>T</span><span>A</span><span>T</span><span>O</span><span>T</span><span>P</span></span>' +
+    '</a>' +
+  '</div>'
+);
